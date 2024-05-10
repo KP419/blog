af436c96070a19daefecabefea28433caefa7b66
Kory Plotts <koryplotts@yahoo.com>
2024-05-09 17:12:45 -0700

    changes

--- posts/migrations/0002_auto_20240506_1809.py
+++ posts/migrations/0002_auto_20240506_1809.py
@@ -2,24 +2,23 @@
 
 from django.db import migrations
 
+
 def populate_status(apps, schemaeditro):
-    entries={
+    entries = {
         "draft": "A post that is not ready to be published",
-        "published":"A post available for all to see",
-        "archived": "An older post available in the archive only"
+        "published": "A post available for all to see",
+        "archived": "An older post available in the archive only",
     }
     Status = apps.get_model("posts", "Status")
     for key, vaule in entries.items():
         status_obj = Status(name=key, description=vaule)
         status_obj.save()
-        
-        
+
+
 class Migration(migrations.Migration):
-    
+
     dependencies = [
         ("posts", "0001_initial"),
     ]
-    
-    operations = [
-        migrations.RunPython(populate_status)
-    ]
+
+    operations = [migrations.RunPython(populate_status)]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.forms import BaseModelForm
 from django.http import HttpResponse
-from django.views.generic import(
+from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
@@ -12,45 +12,54 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Status
 
+
 class PostListView(ListView):
     template_name = "posts/list.html"
     model = Post
-    
+
+
 class DraftPostListView(ListView):
     template_name = "posts/list.html"
     model = Post
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         draft_status = Status.objects.get(name="draft")
-        context["post_list"] = Post.objects.filter(
-            status=draft_status).filter(
-                author=self.request.user).order_by("created_on").reverse()
+        context["post_list"] = (
+            Post.objects.filter(status=draft_status)
+            .filter(author=self.request.user)
+            .order_by("created_on")
+            .reverse()
+        )
         return context
+
 
 class PostDetailView(DetailView):
     template_name = "posts/detail.html"
     model = Post
 
+
 class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = "posts/new.html"
     model = Post
     fields = ["title", "subtitle", "author", "body", "status"]
-    
+
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = "posts/edit.html"
     model = Post
     fields = ["title", "subtitle", "body", "status"]
-    
+
     def test_func(self):
         post = self.get_object()
         return post.author == self.request.user
-    
+
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = "posts/delete.html"
     model = Post
     success_url = reverse_lazy("list")
-    
+
     def test_func(self):
         post = self.get_object()
         return post.author == self.request.user
